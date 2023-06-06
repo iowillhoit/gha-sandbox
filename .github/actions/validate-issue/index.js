2e673a982e8f3db385c1bc5a528c3217d1ea0f57
Eric Willhoit <ewillhoit@salesforce.com>
2023-06-06 16:14:26 -0500

    oops

--- .github/actions/validate-issue/index.js
+++ .github/actions/validate-issue/index.js
@@ -1,5 +1,7 @@
-import { getInput, setOutput, setFailed } from "@actions/core";
-import { context, getOctokit } from "@actions/github";
+// import { getInput, setOutput, setFailed } from "@actions/core";
+// import { context, getOctokit } from "@actions/github";
+
+const { context } = require("@actions/github");
 
 (function () {
   const issue = context.payload.issue;
